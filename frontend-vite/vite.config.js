5f3e523569b46f61f962b2d4500ee303ed69b615
ChasovDS <149188711+ChasovDS@users.noreply.github.com>
2024-11-12 07:59:55 +0300

    upd

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -1,23 +1,24 @@
-// vite.config.js
-
-import { defineConfig } from 'vite'; // Импортируем функцию для определения конфигурации Vite
-import react from '@vitejs/plugin-react'; // Импортируем плагин для поддержки React
-import fs from 'fs'; // Импортируем модуль для работы с файловой системой
-import path from 'path'; // Импортируем модуль для работы с путями файловой системы
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 // Определение пути к сертификатам
-//const sslKey = fs.readFileSync(path.resolve(__dirname, 'localhost.key')); // Читаем закрытый ключ
-//const sslCert = fs.readFileSync(path.resolve(__dirname, 'localhost.crt')); // Читаем сертификат
+// const sslKey = fs.readFileSync(path.resolve(__dirname, 'localhost.key'));
+// const sslCert = fs.readFileSync(path.resolve(__dirname, 'localhost.crt'));
 
 export default defineConfig({
-  plugins: [react()], // Подключаем плагин React
-  server: 
-  {
- //   https: {
-  //    key: sslKey, // Указываем закрытый ключ для HTTPS
-   //   cert: sslCert, // Указываем сертификат для HTTPS
-  //  },
-    host: true, // Разрешает доступ к серверу по сети
-    port: 3000, // Устанавливаем порт, на котором будет работать сервер
+  plugins: [react()],
+  server: {
+    // https: {
+    //   key: sslKey,
+    //   cert: sslCert,
+    // },
+    host: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist', // Укажите выходную директорию
+    sourcemap: true, // Опционально: создание sourcemap для отладки
   },
 });
